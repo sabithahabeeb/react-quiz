6ddcf895e96565b5719af5f4931109b9dc3c3912
sabithahabeeb <sabithahabeeb.sabi@gmail.com>
2023-11-06 15:28:27 +0530

    home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,37 +4,96 @@ import { useNavigate } from 'react-router-dom'
 import Quiz from './Quiz'
 
 function Home() {
-const [startGaame,setStartGame] = useState(false)
-const navigateURL = useNavigate()
-const [name,setName] = useState('')
+  const [startGaame, setStartGame] = useState(false)
+  const navigateURL = useNavigate()
+  const [name, setName] = useState('')
 
 
-const handleStartGame = ()=>{
+  const handleStartGame = () => {
     setStartGame(true)
-}
+  }
 
-const navigate = ()=>{
-  // console.log(`name: ${name}`);
+  const navigate = () => {
+    // console.log(`name: ${name}`);
     navigateURL('/quiz')
-   
-}
+
+  }
   return (
     <>
-<div className='home'>
+      <div className='home'>
 
-    <div className="box">
-    <h1>React Quiz </h1>
-  {/* <button>Start Game</button> */}
-  {startGaame?
-  <div className='start'>
-        <input type="text" placeholder='Enter Youre Name' value={name} onChange={(e)=>setName(e.target.value)} />
-        <button onClick={navigate} >Start</button>
-  </div>:<button onClick={handleStartGame}>Start Game</button>
+        <div className="box">
+          <h1>React Quiz </h1>
+          {/* <button>Start Game</button> */}
+          {startGaame ?
+            <div className='start'>
+              <input type="text" placeholder='Enter Youre Name' value={name} onChange={(e) => setName(e.target.value)} />
+              <button onClick={navigate} >Start</button>
+            </div> : <div><button onClick={handleStartGame}>Start Game</button>
+
+              <h2>INSTRUCTIONS</h2>
+              {/* <div className='div'>
+                  <div>
+
+                  
+                      <h3> 1. Answer 10 Questions</h3>
+                      <p>The Quizz Contain will be 10 questions .Get ready to test youre knowledge have a great time</p>
+                  
+
+
+                    <h3> 2. Time</h3>
+                    <p>Each Questions  is timed for 30 secends,so realax think it through, and enjoy the quiz</p>
+
+                  </div>
+                  <div>
+
+                    <h3> 3. Score</h3>
+                    <p>For each correct answer you'll earn one point . After answering all the questions your total score out of 10 and it will be shown the percentage</p>
+
+
+                    <h3> 4. Play Again Quizz</h3>
+                    <p>Loved this game? Want to prove your expertise? Play again and aim for a higher score!</p>
+
+                  </div>
+                </div> */}
+             <marquee scrollAmount={15}>
+                <div className='div'>
+                  <div className='insta'>
   
-}
+                    <h3> 1. Answer 10 Questions</h3>
+                    <p>The Quizz Contain will be 10 questions. <br /> Get ready to test youre knowledge have <br /> a great time</p>
+  
+                  </div>
+                  <div className='insta'>
+  
+                    <h3> 3. Time</h3>
+                    <p>Each Questions  is timed for 30 secends,<br /> so realax think it through, and enjoy the <br /> quiz</p>
+  
+                  </div>
+                </div>
+                <div className='divv'>
+                  <div className='insta'>
+    
+                    <h3> 2. Score</h3>
+                    <p>For each correct answer you'll earn one <br /> point . After answering all the questions <br /> your total score out of 10 and it will be <br /> shown the percentage</p>
+    
+                  </div>
+                  <div className='insta'>
+    
+                    <h3> 4. Play Again Quizz</h3>
+                    <p>Loved this game? Want to prove your <br /> expertise? Play again and aim for a <br /> higher score!</p>
+                  </div>
+                </div>
+             </marquee>
 
-    </div>
-</div>
+            </div>
+
+
+
+          }
+
+        </div>
+      </div>
 
 
 
--- src/Components/QuestionCard.jsx
+++ src/Components/QuestionCard.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from 'react'
 import './question.css'
 import Result from './Result'
+import Timer from './Timer';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function QuestionCard({setFinalResult,score,setScore,questions,currentQuestion,setCurrentQuestion,name}) {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [stop,setStop] = useState(false)
   const optionClicked = (isCorrect)=>{
     if(isCorrect){
 setScore(score + 1)
-
+// alert("answer is correct")
+toast.success("Correct")
+    }else{
+      toast.error("Wrong")
     }
     if (currentQuestion + 1 < questions.length) {
       setCurrentQuestion(currentQuestion + 1);
@@ -23,9 +30,12 @@ setScore(score + 1)
     <>
      {/* current score */}
      <h2>Current Score: {score}</h2>
-     <button onClick={toggleMode} style={{width:'10px',height:'10px',backgroundColor:'transparent'}}>
-          {isDarkMode ? <i class="fa-solid fa-sun  fa-xl " style={{color:'white'}}></i> :  <i class="fa-solid fa-moon  fa-xl " style={{color:''}}></i>}
-        </button>
+ <div className='timer'>
+       <button onClick={toggleMode} style={{width:'10px',height:'10px',backgroundColor:'transparent'}}>
+            {isDarkMode ? <i class="fa-solid fa-sun  fa-xl " style={{color:'white'}}></i> :  <i class="fa-solid fa-moon  fa-xl " style={{color:''}}></i>}
+          </button>
+          <div className='number'><Timer currentQuestion={currentQuestion} setStop={setStop}/></div>
+ </div>
 
     <div className={isDarkMode?'dark-mode question-card':'light-mode question-card'}>
       <span style={{color:'crimson'}}>sabitha</span>
@@ -42,6 +52,10 @@ setScore(score + 1)
             
         </ul>
     </div>
+    <ToastContainer
+    position="top-center"
+    autoClose={500}
+    />
     </>
   )
 }
